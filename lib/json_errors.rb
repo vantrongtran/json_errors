3b0804d534694f32b92b28241986d1c50c0c0bf4
Pham Van Hoang B <pham.van.hoang-b@sun-asterisk.com>
2023-03-02 09:53:49 +0700

    Create Global Errors to store all defined errors from setting's file

--- lib/json_errors.rb
+++ lib/json_errors.rb
@@ -1,6 +1,36 @@
 # frozen_string_literal: true
 
-require_relative "json_errors/version"
+require 'json_errors/version'
+require 'json_errors/configuration'
+require 'yaml'
 
 module JsonErrors
+  extend ActiveSupport::Concern
+  extend JsonErrors::Configuration.new(
+    default_error_status: :bad_request
+  )
+
+  def self.setup
+    yield self unless @_ran_once
+    @_ran_once = true
+  end
+
+  def self.load_config_files!
+    code_file = ::Rails.root.join('config', 'error_codes.yml')
+    YAML.load_file(code_file) if File.exist?(code_file)
+  end
+
+  def self.load_and_set_settings
+    JsonErrors.load_error_setting
+  end
+
+  def self.reload!
+    JsonErrors.load_error_setting
+  end
+
+  def self.load_error_setting
+    @error_setting = JsonErrors.load_config_files!
+  end
 end
+
+require('json_errors/railtie') if defined?(::Rails::Railtie)
---
+++ lib/json_errors/configuration.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+module JsonErrors
+  class Configuration < Module
+    def initialize(**attributes)
+      attributes.each do |name, default|
+        define_reader(name, default)
+        define_writer(name)
+      end
+      define_reader(:error_setting, {})
+      define_writer(:error_setting)
+    end
+
+    private
+
+    def define_reader(name, default)
+      variable = :"@#{name}"
+
+      define_method(name) do
+        if instance_variable_defined?(variable)
+          instance_variable_get(variable)
+        else
+          default
+        end
+      end
+    end
+
+    def define_writer(name)
+      variable = :"@#{name}"
+
+      define_method("#{name}=") do |value|
+        instance_variable_set(variable, value)
+      end
+    end
+  end
+end
